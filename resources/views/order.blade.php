e58a47d8b61d47fe19edbb6595c40884c9b5bf83
Renato Lopez <lopez.renato@gmail.com>
2017-11-23 19:34:20 -0500

    Actualizacion de la vista order

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -14,68 +14,42 @@
                       
                       
                                 
-
-                    $personalidada=utf8_encode("EL Ministerio de Educaci&oacute;n del Ecuador ha declarado recientemente el inicio de la toma de ex&oacute;menes psicom&oacute;tricos para los aspirantes a docentes del proceso QSM6. Proceso que ofrece cerca de 20 mil plazas de trabajo con nombramiento para docentes de las diferentes especialidades a nivel nacional.");
+                    $personalidada=utf8_encode("EL Ministerio de Educación del Ecuador ha declarado recientemente el inicio de la toma de exámenes psicométricos para los aspirantes a docentes del proceso QSM6. Proceso que ofrece cerca de 20 mil plazas de trabajo con nombramiento para docentes de las diferentes especialidades a nivel nacional.");
                     $personalidad=("");
-                    $personalidad2=utf8_encode("Es por ello que se exhorta a los interesados a no perder esta oportunidad, que pudiera ser la &oacute;ltima, de participar para obtener una plaza de trabajo dentro del Magisterio.");
+                    $personalidad2=utf8_encode("Es por ello que se exhorta a los interesados a no perder esta oportunidad, que pudiera ser la última, de participar para obtener una plaza de trabajo dentro del Magisterio.");
                     
-                    $personalidad4=utf8_encode("Al aplicar a nuestro servicio de capacitaci&oacute;n y entrenamiento virtual pr&oacute;ctico Me Capacito Ecuador, contar&oacute;s con la asesor&oacute;a de profesionales expertos en el tema de pruebas psicom&oacute;tricas y de conocimientos, quienes han desarrollado cuidadosamente el banco de preguntas para cada participaci&oacute;n, y as&oacute; evitar errores que podr&oacute;an significar la anulaci&oacute;n de tu prueba.");
-                    $personalidadfin=utf8_encode("El organismo oficial declar&oacute; que las pruebas empezar&oacute;n a partir de 9 de Noviembre durante los siguientes 2 meses.");
-                    $personalidadfin2=utf8_encode("El servicio de YoSiQuierosermaestro.com y MeCapacitoEcuador.com en ning&oacute;n momento representa al organismo oficial, solo presta un servicio serio y eficiente de capacitaci&oacute;n pr&oacute;ctica para las diferentes pruebas que tomar&oacute; del Ministerio. El contratar nuestros servicios garantiza su correcta capacitaci&oacute;n y sobresaliente participaci&oacute;n en el proceso. Los datos suministrados por medio de esta p&oacute;gina web tienen como finalidad &oacute;nicamente informar.");
+                    $personalidad4=utf8_encode("Al aplicar a nuestro servicio de capacitación y entrenamiento virtual práctico Me Capacito Ecuador, contarás con la asesoría de profesionales expertos en el tema de pruebas psicométricas y de conocimientos, quienes han desarrollado cuidadosamente el banco de preguntas para cada participación, y así evitar errores que podrían significar la anulación de tu prueba.");
+                    $personalidadfin=utf8_encode("El organismo oficial declaró que las pruebas empezarán a partir de 9 de Noviembre durante los siguientes 2 meses.");
+                    $personalidadfin2=utf8_encode("El servicio de YoSiQuierosermaestro.com y MeCapacitoEcuador.com en ningún momento representa al organismo oficial, solo presta un servicio serio y eficiente de capacitación práctica para las diferentes pruebas que tomará del Ministerio. El contratar nuestros servicios garantiza su correcta capacitación y sobresaliente participación en el proceso. Los datos suministrados por medio de esta página web tienen como finalidad únicamente informar.");
                      
                     
                     
-                    $personalidadven=utf8_encode("Nuestros cursos y pruebas de capacitaci&oacute;n se adaptan a tu tiempo y espacio, puedes acceder en el horario que se ajuste mejor a ti y desde el lugar que te encuentres. ");
-                    $personalidadBAN=utf8_encode("Simulador de ex&oacute;men: Contamos con bancos de preguntas para repasar antes de presentar el examen. Se provee feedback sobre las preguntas, ayud&oacute;ndote a mejorar y reforzar tus conocimientos. ");
+                    $personalidadven=utf8_encode("Nuestros cursos y pruebas de capacitación se adaptan a tu tiempo y espacio, puedes acceder en el horario que se ajuste mejor a ti y desde el lugar que te encuentres. ");
+                    $personalidadBAN=utf8_encode("Simulador de exámen: Contamos con bancos de preguntas para repasar antes de presentar el examen. Se provee feedback sobre las preguntas, ayudándote a mejorar y reforzar tus conocimientos. ");
                     $personalidadesp=utf8_encode("Sigue los tips y recomendaciones para facilitarte el entrenamiento");
-                    $personalidadfin6=utf8_encode("Ingresa a los simuladores de las pruebas psicom&oacute;tricas generando una orden de pedido y el pago correspondiente, si tienes alguna duda puedes contactarte con nosotros a info@yosiquierosermaestro.com o siguenos en facebook ");
-
-                    $personalidada=utf8_encode("EL Ministerio de Educaci&oacute;n del Ecuador ha declarado recientemente el inicio de la toma de ex&aacute;menes psicom&eacute;tricos para los aspirantes a docentes del proceso QSM6. Proceso que ofrece cerca de 20 mil plazas de trabajo con nombramiento para docentes de las diferentes especialidades a nivel nacional.");
-                    $personalidad=("");
-                    $personalidad2=utf8_encode("Es por ello que se exhorta a los interesados a no perder esta oportunidad, que pudiera ser la Ãºltima, de participar para obtener una plaza de trabajo dentro del Magisterio.");
-                    
-                    $personalidad4=utf8_encode("Al aplicar a nuestro servicio de capacitaci&oacute;n y entrenamiento virtual pr&aacute;ctico Me Capacito Ecuador, contar&aacute;s con la asesorÃ­a de profesionales expertos en el tema de pruebas psicom&eacute;tricas y de conocimientos, quienes han desarrollado cuidadosamente el banco de preguntas para cada participaci&oacute;n, y asÃ­ evitar errores que podrÃ­an significar la anulaci&oacute;n de tu prueba.");
-                    $personalidadfin=utf8_encode("El organismo oficial declar&oacute; que las pruebas empezar&aacute;n a partir de 9 de Noviembre durante los siguientes 2 meses.");
-                    $personalidadfin2=utf8_encode("El servicio de YoSiQuierosermaestro.com y MeCapacitoEcuador.com en ningÃºn momento representa al organismo oficial, solo presta un servicio serio y eficiente de capacitaci&oacute;n pr&aacute;ctica para las diferentes pruebas que tomar&aacute; del Ministerio. El contratar nuestros servicios garantiza su correcta capacitaci&oacute;n y sobresaliente participaci&oacute;n en el proceso. Los datos suministrados por medio de esta p&aacute;gina web tienen como finalidad Ãºnicamente informar.");
-                     
-                    
-                    
-                    $personalidadven=utf8_encode("Nuestros cursos y pruebas de capacitaci&oacute;n se adaptan a tu tiempo y espacio, puedes acceder en el horario que se ajuste mejor a ti y desde el lugar que te encuentres. ");
-                    $personalidadBAN=utf8_encode("Simulador de ex&aacute;men: Contamos con bancos de preguntas para repasar antes de presentar el examen. Se provee feedback sobre las preguntas, ayud&aacute;ndote a mejorar y reforzar tus conocimientos. ");
-                    $personalidadesp=utf8_encode("Sigue los tips y recomendaciones para facilitarte el entrenamiento");
-                    $personalidadfin6=utf8_encode("Ingresa a los simuladores de las pruebas psicom&eacute;tricas generando una orden de pedido y el pago correspondiente, si tienes alguna duda puedes contactarte con nosotros a info@yosiquierosermaestro.com o siguenos en facebook ");
-
+                    $personalidadfin6=utf8_encode("Ingresa a los simuladores de las pruebas psicométricas generando una orden de pedido y el pago correspondiente, si tienes alguna duda puedes contactarte con nosotros a info@yosiquierosermaestro.com o siguenos en facebook ");
                ?>
 
 
 <div class="single_stuff wow fadeInDown">
-    <div class="single_stuff_img"> <a href="#"><br><br></a> </div>
+      <div class="single_stuff_img"> <a href="#"><img src="images/bannerPromocionYoSiQuiero1.png" alt=""></a> </div>
              
               <div class="single_stuff_article">
                 <div class="single_sarticle_inner"> 
                   <div class="stuff_article_inner">
                       
-
                       <div>
-<iframe width="100%" height="230px" src="https://www.youtube.com/embed/jw-P4K-bVyc" frameborder="0" allowfullscreen></iframe>
+
 </div>
                          <h2>Funcionamiento de la plataforma virtual de entrenamiento</h2>
                                                  
-
-                      <div><img style="visibility: visible; ;width: 100%;height: 230px;" src="images/sala.jpg" alt=""></div>
-                         <h2>Bienvenido a la plataforma virtual de entrenamiento</h2>
-                         <p>{!!$personalidada!!}</p>
-                         
-
                          
                          <p style="color:red"><b >{!!$personalidad4!!} </b></p>
                               
                     
                     <p>{!!$personalidadfin6!!} </p>
                     
-
                      <h2>Suscribete al simulador de entrenamiento online</h2>                    
-                     
 						
 						<p>
 
@@ -201,6 +175,7 @@
 	<script>
             
          
+    
             function validate() {
                 
                  //  $( "#submit" ).prop( "disabled", true );
@@ -224,7 +199,6 @@
         return true;
 }
 }
-
 
 
 
